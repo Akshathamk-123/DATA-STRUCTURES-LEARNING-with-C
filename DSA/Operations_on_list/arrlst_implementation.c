f23dc5016e9bb9c598f67500572f6a66cabdd85c
Akshatha MK <92522733+Akshathamk-123@users.noreply.github.com>
2022-08-18 21:15:06 +0530

    Update arrlst_implementation.c

--- DSA/Operations_on_list/arrlst_implementation.c
+++ DSA/Operations_on_list/arrlst_implementation.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include "arrlst_header.h"
 void init(ARRLST *pal)
 {
     pal->index= -1;
@@ -9,9 +11,47 @@ int append(ARRLST *pal,int e)
         return 0;
     pal->index++;
     pal->a[pal->index]=e;
+    return 1;
 }
 
 int delLast(ARRLST *pal,int *ele)
 {
-    
+    if(pal->index==-1) return 0;
+    *ele=pal->a[pal->index];
+    pal->index--;
+    return 1; 
 }
+
+void display(ARRLST *pal)
+{
+    if(pal->index==-1) return;
+    for(int i=0;i<=pal->index;i++)
+    {
+        printf("%d\t",pal->a[i]);
+    }
+    printf("\n");
+}
+
+int prepend(ARRLST *pal,int ele)
+{
+    if(pal->index==MAX-1) return 0;
+    for (int i =pal->index; i>=0; i--)
+    {
+       pal->a[i+1]=pal->a[i];
+    }
+    pal->index++;
+    pal->a[0]=ele;
+    return 1;     
+}
+
+int deletefront(ARRLST *pal,int* ele)
+{
+    if(pal->index==-1) return 0;
+    *ele=pal->a[0];
+    for(int i=1; i<=pal->index; i--)
+    {
+        pal->a[i-1]=pal->a[i];
+    }
+    return 1;
+}
+
