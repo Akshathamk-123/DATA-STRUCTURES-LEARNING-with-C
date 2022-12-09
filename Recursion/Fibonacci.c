8ac7a9c03fd708fc6069cd85728daebf5ca4f0fa
Akshatha MK <92522733+Akshathamk-123@users.noreply.github.com>
2022-12-09 10:45:20 +0530

    Add files via upload

---
+++ Recursion/Fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int fibonacci(int n);
+int main()
+{
+    printf("Enter the value of n to find the Fibonacci series");
+    int n;
+    scanf("%d",&n);
+    printf("The Fibonacci series is as follows: \t");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\t",fibonacci(i));
+    }
+}
+
+int fibonacci(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==2 || n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
